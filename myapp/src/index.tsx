ad31b0302bc20bb241d52a77f922139e7c0bfbba
Jacob Pascua <pascuajakecreed@gmail.com>
2023-03-14 01:45:09 -0400

    stubbed out

---
+++ myapp/src/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import * as functions from "firebase-functions"
+import express from 'express';
+import cors from 'cors';
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.use("/", exampleRoutes)
+
+export const api = functions.https.onRequest(app)
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
