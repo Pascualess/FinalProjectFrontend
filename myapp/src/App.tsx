24db2a58663d414d8df9563e134d575504ed915d
Jacob Pascua <pascuajakecreed@gmail.com>
2023-03-19 19:57:34 -0400

    Auth seems to be working

--- myapp/src/App.tsx
+++ myapp/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AuthContextProvider from './context/AuthContextProvider';
+import { Authentication } from './components/Authentication';
 
 function App() {
   return (
     <div className="App">
-      <p>Hello</p>
+      <AuthContextProvider>
+        <Authentication />
+      </AuthContextProvider>
     </div>
   );
 }
