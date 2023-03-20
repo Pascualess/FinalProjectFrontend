7df3e956f246bdaba26d1c546eb0a558b31355d9
Jacob Pascua <pascuajakecreed@gmail.com>
2023-03-20 19:03:20 -0400

    routes

--- myapp/src/App.tsx
+++ myapp/src/App.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import AuthContextProvider from './context/AuthContextProvider';
-import { Authentication } from './components/Authentication';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
+import AuthContextProvider from "./context/AuthContextProvider";
+import { Authentication } from "./components/Authentication";
 
 function App() {
   return (
-    <div className="App">
-      <AuthContextProvider>
-        <Authentication />
-      </AuthContextProvider>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <AuthContextProvider>
+            <Route path="/login" element={<Authentication />} />
+            <Route path="*" element={<Navigate to={"/login"} />} />
+          </AuthContextProvider>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
